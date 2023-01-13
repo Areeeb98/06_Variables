3e72346b6a3650053065057cd55fa2ffa71cefba
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-14 00:46:21 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,91 @@
+"""
+Day: Saturday
+Date: 14th jan 2023
+Topic: Variables
+       valid or not valid variables
+       4 different ways to assign values in variables
+Time: 12AM
+Author: Areeb Ahmed
+"""
+
+'''The following are the variables which are valid'''
+a=1
+A=2
+_b=3
+_1=4
+c_a=5
+v_3=6
+_=7
+x_y3=8
+r6=9
+t_4_d=10
+
+# print(a)
+# print(A)
+# print(_b)
+# print(_1)
+# print(c_a)
+# print(v_3)
+# print(_)
+# print(x_y3)
+# print(r6)
+# print(t_4_d)
+
+# print('Areeb'.isidentifier())
+# print('4ello'.isidentifier())
+# print('_Ahmed07'.isidentifier())
+
+# a='python'
+# print(a.isidentifier())
+# b='hello'
+# print(b.isidentifier())
+# c='23hh'
+# print(c.isidentifier())
+# d='p_y'
+# print(d.isidentifier())
+# e='_'
+# print(e.isidentifier())
+# f='_@45'
+# print(f.isidentifier())
+# g='a b'
+# print(g.isidentifier())
+
+"""ways to assign the variables"""
+# First way to assign the values
+a=34
+b=78
+c=92
+d=22
+e=98
+# print(c)
+# print(e)
+# print(b)
+# print(d)
+# print(a)
+
+# print(a); print(b); print(c); print(d); print(e)
+# print(a,b,c,d,e,b,d,a,e,c)
+# print(a), print(b), print(c), print(d), print(e)
+
+"""second way to assign values"""
+# a=1; b=2; c=3; d=4
+# print(a); print(b); print(c); print(d)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(a,d,b,c)
+
+"""Third way to assign values"""
+# a,b,c,d=1,2,3,4,
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(a,b,c,d)
+# print(b); print(c); print(d)
+
+"""Fourth way to assign values"""
+a=d=7; b,c,e=1,3,5
+# print(a,b,c,d,e)
+print(a); print(b); print(c); print(d)
